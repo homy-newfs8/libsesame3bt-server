f262b6684acbc234df41afe0ed1fe79d353f8eb6
Naoki Sawada <1412630+nao-pon@users.noreply.github.com>
2026-08-15 11:10:39 +0900

    feat: expose version tag configuration (#1)

--- lib/libsesame3bt-server/SesameServer.h
+++ lib/libsesame3bt-server/SesameServer.h
@@ -7,6 +7,7 @@
 #include <functional>
 #include <optional>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 
 namespace libsesame3bt {
@@ -49,6 +50,7 @@ class SesameServer : private NimBLEServerCallbacks, private NimBLECharacteristic
 	bool send_mecha_status(const NimBLEAddress* address, const Sesame::mecha_status_5_t& status);
 	void set_mecha_setting(const Sesame::mecha_setting_5_t& setting) { core.set_mecha_setting(setting); }
 	void set_mecha_status(const Sesame::mecha_status_5_t& status) { core.set_mecha_status(status); }
+	void set_version_tag(std::string_view tag) { core.set_version_tag(tag); }
 	void set_auto_send_flags(auto_send::flags flags) { core.set_auto_send_flags(flags); }
 	bool has_session(const NimBLEAddress& addr) const;
 	void disconnect(const NimBLEAddress& addr);
